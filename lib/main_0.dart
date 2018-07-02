327514d6d4a81918bb93271cdd9ed46b57b52f3d
luis_mantilla <lmmantilla9350@gmail.com>
2018-07-01 22:49:48 -0400

    first commit

---
+++ lib/main_0.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HelloRectangle extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Center(
+      child: Container(
+        color: Colors.green,
+        height: 400.0,
+        width: 400.0,
+        child: Center(
+          child: Text(
+            'Hello!!!',
+            style: TextStyle(
+                fontSize: 40.0,
+                color: Colors.white
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main(){
+  //print('Hello!!');
+  runApp(
+      MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Scaffold(
+            appBar: AppBar(
+              title: Text('Hello Rectangle'),
+            ),
+            body: HelloRectangle(),
+          )
+      )
+  );
+
+}
